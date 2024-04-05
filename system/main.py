59d89162fefefc61d9d7c8b0ba00b4ad0c954641
dzx666 <3020713795@qq.com>
2024-04-05 21:34:37 +0800

    blog系统化

---
+++ system/main.py
@@ -0,0 +1,220 @@
+import os
+import ctypes
+import traceback
+import sys
+import datetime
+import json
+import base64
+import shutil
+from Crypto.Cipher import AES
+from typing import Literal,Union
+
+# str不是16的倍数那就补足为16的倍数
+def add_to_16(message:str):
+    while len(message) % 16 != 0:
+        message = str(message)
+        message += '\0'
+        # message = str(message)
+    return message.encode('utf-8')  # 返回bytes
+
+
+# 加密方法
+def encrypt_oracle(message: str,key_pri: str):
+    '''
+    加密函数，传入明文 & 秘钥，返回密文；
+    :param message: 明文
+    :param key_pri: 秘钥
+    :return:encrypted  密文
+    '''
+    # 初始化加密器
+    aes = AES.new(add_to_16(key_pri), AES.MODE_ECB)
+    # 将明文转为 bytes
+    message_bytes = message.encode('utf-8')
+    # 长度调整
+    message_16 = add_to_16(message_bytes)
+    #先进行aes加密
+    encrypt_aes = aes.encrypt(message_16)
+    #用base64转成字符串形式
+    encrypt_aes_64 = base64.b64encode(encrypt_aes)
+    return encrypt_aes_64.decode("utf-8")
+
+
+# 解密方法
+def decrypt_oralce(message,key_pri):
+    '''
+    解密函数，传入密文 & 秘钥，返回明文；
+    :param message: 密文
+    :param key_pri: 秘钥
+    :return: encrypted 明文
+    '''
+    # 初始化加密器
+    aes = AES.new(add_to_16(key_pri), AES.MODE_ECB)
+    #优先逆向解密base64成bytes
+    message_de64 = base64.b64decode(message)
+    # 解密 aes
+    message_de64_deaes = aes.decrypt(message_de64)
+    message_de64_deaes_de = message_de64_deaes.decode('utf-8')
+    return message_de64_deaes_de
+
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+    
+def update_index():
+    '''更新页面索引'''
+    res = {}
+    for dir in os.listdir("pages"):
+        with open(os.path.join("pages",dir,"attribute.json"),"r",encoding="utf-8") as f:
+            datar = json.load(f)
+        print("标题：{0}\t是否上锁：{1}\t标签：{2}".format(datar["title"],str(datar["is_locked"]),str(datar["tags"])))
+        res[datar["title"]] = datar
+    datar = json.dumps(res, sort_keys=True, indent=4, separators=(',', ': '),ensure_ascii=False)
+    with open("index.json","w",encoding="utf-8") as f:
+        f.write(datar)
+    print("完成 共"+str(len(res))+"个项目")
+
+def edit_page():
+    '''操作页面'''
+
+def new_page():
+    '''新建页面'''
+    title = ""
+    is_exit = False
+    while title == "":
+        title = input("标题（按0撤回）：")
+        if title == "0":
+            is_exit = True
+            break
+        dir = os.path.join("pages",title)
+        if title == "":
+            print("不能为空！")
+        if os.path.exists(dir):
+            print("该页面已存在！")
+            title = ""
+    if is_exit:
+        return -1
+    tags = []
+    while True:
+        tag = input("标签（按0结束）：")
+        if tag == "0":
+            break
+        else:
+            if tag == "" or tag == " ":
+                print("不能为空")
+            else:
+                tags.append(tag)
+    while True:
+        locked = input("是否上锁(y/n)：")
+        if locked == "y":
+            locked = True
+            password = ""
+            while password == "":
+                password = input("密码：")
+                if password == "":
+                    print("不能为空！")
+            break
+        elif locked == "n":
+            locked = False
+            break
+    f = open("temp.html","w",encoding="utf-8")
+    f.write('<!DOCTYPE html><html lang="zh-CN"><head><title>{title}</title><link rel="stylesheet" type="text/css" href="../style.css"><meta name="viewport" content="width=device-width, initial-scale=1"><meta charset="utf-8"></head><body><h1>{title}</h1><p>最后一次更新时间：{date}</p>\n<!-- 请在以下写入代码 -->\n\n'.format(title=title,date=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+    f.close()
+    os.system('start "" "D:/Microsoft VS Code/Code.exe" temp.html')
+    print("请修改后保存")
+    os.system("pause")
+    f = open("temp.html","r",encoding="utf-8")
+    res = f.readlines()[2:]
+    f.close()
+    os.remove("temp.html")
+    r = ""
+    for i in res:
+        r += i
+    os.mkdir(dir)
+    if locked:
+        r = encrypt_oracle("locked"+r,password)
+    with open(os.path.join(dir,"content.html"),"w",encoding="utf-8") as f:
+        f.write(r)
+    datar = json.dumps({"title":title,"tags":tags,"is_locked":locked,"create_time":datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),"last_edit_time":datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}, sort_keys=True, indent=4, separators=(',', ': '),ensure_ascii=False)
+    with open(os.path.join(dir,"attribute.json"),"w",encoding="utf-8") as f:
+        f.write(datar)
+
+def apply():
+    '''部署网站'''
+    
+    # 删除原有文件
+    folder_path = "../pages"
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f'Failed to delete {file_path}. Reason: {e}')
+    # 创建文件
+    with open("index.json","r",encoding="utf-8") as f:
+        data:dict[str,dict[Literal["title","tags","is_locked","create_time","last_edit_time"],Union[str,bool,list[str]]]] = json.load(f)
+    for i in data:
+        with open(os.path.join("../pages/",data[i]["title"]+".html"),"w",encoding="utf-8") as f:
+            if data[i]["is_locked"]:
+                with open(os.path.join("pages",data[i]["title"],"content.html"),"r",encoding="utf-8") as f1:
+                    f.write(r'<!DOCTYPE html><html lang="zh-CN"><head><title>{title}</title><link rel="stylesheet" type="text/css" href="../style.css"><meta name="viewport" content="width=device-width, initial-scale=1"><meta charset="utf-8"><script src="../system/crypto-js.min.js"></script></head><body><h1>{title}</h1><a href="../blogs.html">返回</a><p>最后一次更新时间：{date}</p><p>创建时间：{create}</p><div id="tip"><p>这个文档被加密了，需要密码...</p><br/><span>密码：</span><input id="2"><button onclick="sure();">确定</button></div><div id="1"></div><script>function sure(){{var akey=document.getElementById("2").value;while(akey.length%16!=0){{akey+="\0"}};var key=CryptoJS.enc.Utf8.parse(akey);const mode=CryptoJS.mode.ECB;const padding=CryptoJS.pad.Pkcs7;var decryptedText=CryptoJS.AES.decrypt("{base64}",key,{{mode,padding}});var a=decodeURIComponent(CryptoJS.enc.Utf8.stringify(decryptedText).replace(/\\x/g,"%"));document.getElementById("1").innerHTML=a;if(document.getElementById("1").innerHTML.slice(2,8)=="locked"){{document.getElementById("tip").innerHTML="";}}document.getElementById("1").innerHTML=document.getElementById("1").innerHTML.replace(/\\n/g,"").slice(8,-1);}}</script></body></html>'.format(title=data[i]["title"],date=data[i]["last_edit_time"],create=data[i]["create_time"],base64=f1.read()))
+            else:
+                f.write('<!DOCTYPE html><html lang="zh-CN"><head><title>{title}</title><link rel="stylesheet" type="text/css" href="../style.css"><meta name="viewport" content="width=device-width, initial-scale=1"><meta charset="utf-8"></head><body><h1>{title}</h1><a href="../blogs.html">返回</a><p>最后一次更新时间：{date}</p><p>创建时间：{create}</p>\n'.format(title=data[i]["title"],date=data[i]["last_edit_time"],create=data[i]["create_time"]))
+                with open(os.path.join("pages",data[i]["title"],"content.html"),"r",encoding="utf-8") as f1:
+                    f.write(f1.read())
+                f.write("</body></html>")
+    # 创建索引
+    with open("../blogs.html","w",encoding="utf-8") as f:
+        f.write('<!DOCTYPE html><html lang="zh-CN"><head><title>一些bb</title><link rel="stylesheet" type="text/css" href="style.css"><meta name="viewport" content="width=device-width, initial-scale=1"><meta charset="utf-8"></head><body><h1>一些bb</h1><p>最后一次更新时间：{date}</p><ul style="list-style-type: none;">'.format(date=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+        for i in data:
+            tag = ""
+            for o in data[i]["tags"]:
+                tag+=o+" "
+            f.write("<li><div class='head'><a href='pages/{title}.html'>{title}</a></div><div class='foot'> {locked} 更新时间：{date} 标签：{tags}</div></li>".format(title=data[i]["title"],locked="已加密" if data[i]["is_locked"] else "公开",date=data[i]["last_edit_time"],tags=tag))
+        f.write("</ul></body></html>")
+if __name__=="__main__":
+    if is_admin():
+        try:
+            os.chdir(os.path.dirname(os.path.abspath(__file__)))
+            while True:
+                print()
+                cmd = None
+                print("="*10)
+                print("这是网站后台")
+                print("1.页面管理")
+                print("2.网站部署")
+                print("="*10)
+                cmd = input("输入指令：")
+                print()
+                if cmd == "1":
+                    while True:
+                        print()
+                        print("当前位置：页面管理")
+                        print("="*10)
+                        print("0.返回上一级")
+                        print("1.操作已有页面")
+                        print("2.更新页面索引")
+                        print("3.创建页面")
+                        print("="*10)
+                        cmd = input("输入指令：")
+                        print()
+                        if cmd == "0":
+                            break
+                        elif cmd == "1":
+                            edit_page()
+                        elif cmd == "2":
+                            update_index()
+                        elif cmd == "3":
+                            new_page()
+                elif cmd == "2":
+                    apply()
+        except Exception as e:
+            traceback.print_exc()
+            input()
+    else:
+        ctypes.windll.shell32.ShellExecuteW(None,"runas",sys.executable,__file__,None,1)
